a76c95635c7fd721f010dfbf69b4060055eade4d
AndreyCodes <70113676+AndreyCodes@users.noreply.github.com>
2022-01-31 18:22:26 +0300

    Add files via upload

---
+++ foo.cpp
@@ -0,0 +1,24 @@
+﻿
+
+#include "foo.h"
+
+int sum(int a, int b)
+{
+	return a + a + b;
+}
+
+
+	State::State(int a_, int b_) : a{a_}, b{b_}
+	{
+
+	}
+	int State::Sum()
+	{
+		return a + a + b;
+	}
+	State::~State()
+	{
+		a = 0;
+	}
+
+
---
+++ foo.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+extern "C" int sum(int a, int b);
+
+
+class State
+{
+	int a;
+	int b;
+public:
+	State(int a_, int b_);
+	int Sum();
+	~State();
+};
